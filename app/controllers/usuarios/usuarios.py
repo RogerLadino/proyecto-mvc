2acc5c38c7198b7dc2071c34636840195d40f1e8
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-07-01 20:32:58 -0500

    Eliminacion archivo usuarios.py

--- app/controllers/usuarios/usuarios.py
+++
@@ -1,6 +0,0 @@
-# Este archivo debe contener las siguientes rutas:
-
-# POST - GET /usuario/registrar -- Registrar usuario
-# POST - GET /usuario/iniciar-sesion - Iniciar sesión
-
-### En lugar de poner el email en la sesión, poner el id del usuario
