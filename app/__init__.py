c38b56928a1beeb6b79596b412ceffffbdda2f6f
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-07-01 20:39:52 -0500

    Feat: Integrado nuevo controller de usuario en la app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask
+# app/__init__.py
 import pymysql.cursors
+from flask import Flask
 from config import Config
+from flask_bcrypt import Bcrypt
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     connection = pymysql.connect(
         host=app.config['MYSQL_HOST'],
@@ -14,7 +15,23 @@ def create_app():
         database=app.config['MYSQL_DB'],
         cursorclass=pymysql.cursors.DictCursor
     )
-
     app.connection = connection
 
+    Bcrypt(app)
+
+    # Registrar Blueprints
+    with app.app_context():
+        from .controllers.main import main_bp
+        app.register_blueprint(main_bp)
+
+    with app.app_context():
+        # Aquí registramos nuestro controlador
+        from .controllers.usuarios_controller import usuario_bp
+        app.register_blueprint(usuario_bp)
+
+    # Registrar Blueprints
+        with app.app_context():
+            from .controllers.main import main_bp
+            app.register_blueprint(main_bp)
+        
     return app
--- app/controllers/main.py
+++ app/controllers/main.py
@@ -1,1 +1,11 @@
-# Este controlador debe mostrar el index.html en la ruta /
+# app/controllers/main.py
+
+from flask import Blueprint, render_template
+
+# Creamos un nuevo Blueprint para las rutas principales
+main_bp = Blueprint('main_bp', __name__, template_folder='../../templates')
+
+@main_bp.route('/')
+def index():
+    """Muestra la página de inicio (index.html)."""
+    return render_template('index.html')
